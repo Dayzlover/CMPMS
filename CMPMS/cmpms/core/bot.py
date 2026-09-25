4367acfa952fcbb5775c765025cb6c75f5919ad4
Dayzlover <132408901+Dayzlover@users.noreply.github.com>
2026-09-25 16:21:34 -0500

    Add files via upload

--- CMPMS/cmpms/core/bot.py
+++ CMPMS/cmpms/core/bot.py
@@ -26,21 +26,19 @@ class CMPMSBot(commands.Bot):
                 await self.load_extension(f"cmpms.cogs.{file[:-3]}")
                 self.logger.info(f"[COG] Loaded: {file[:-3]}")
 
-        # Sync slash commands
+               # Sync slash commands
         synced_commands = await self.tree.sync()
 
         self.logger.info("[SYSTEM] Slash commands synchronized.")
 
         for command in synced_commands:
-             self.logger.info(
-                f"[COMMAND] /{command.name}"
-    )
+            self.logger.info(f"[COMMAND] /{command.name}")
 
-        if hasattr(command, "commands"):
-            for subcommand in command.commands:
-                self.logger.info(
-                    f"[COMMAND] /{command.name} {subcommand.name}"
-            )
+            if hasattr(command, "commands"):
+                for subcommand in command.commands:
+                    self.logger.info(
+                        f"[COMMAND] /{command.name} {subcommand.name}"
+                    )
 
     async def on_ready(self):
         self.logger.info("=" * 50)
